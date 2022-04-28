92b320d286c44d1cb2ae4b4647f5277a3549ddd5
marcoglaus-teko <marco.glaus2@edu.teko.ch>
2022-04-28 15:10:42 +0200

    test git

--- src/ch/teko/prg2/tag01/input/Main.java
+++ src/ch/teko/prg2/tag01/input/Main.java
@@ -3,5 +3,6 @@ package ch.teko.prg2.tag01.input;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Test Git");
     }
 }
